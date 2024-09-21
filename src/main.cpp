3c0c576be382d6f545bb8db93627872e38ee4df7
oleksiy1990@gmail.com <oleksiy1990@gmail.com>
2024-09-21 23:12:07 +0200

    Code to change ESP8266 baud from 115200 to 9600

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+String response;
+const char end_char{'\n'};
+char buf[50];
 
 void setup() {
+  Serial.begin(115200);
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
+  /*
+  Serial.write("AT+GMR\r\n");
+  Serial.flush();
+  delay(200);
+  response = Serial.readStringUntil(end_char);
+  response.toCharArray(buf, 50);
+  Serial.write(buf);
+  Serial.write('\n');
+  Serial.flush();
+  delay(2000);
+  */
+  Serial.write("AT+UART_DEF=9600,8,1,0,0\r\n");
+  Serial.flush();
+  delay(200);
+  //response = "";
+ 
+  response = Serial.readStringUntil(end_char);
+  response.toCharArray(buf, 50);
+  Serial.write(buf);
+  Serial.write('\n');
+  Serial.flush();
+  memset(buf,0,sizeof(buf));
+  delay(2000);
+  
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
